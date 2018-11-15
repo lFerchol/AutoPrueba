f27ebaadca9a80efee37023fab771826c0994de9
lFerchol <cadesluisxTo22@gmail.com>
2018-11-15 11:26:05 -0600

    Clase principal
    Se agregó la clase principal para el
    "Hola Mundo con MAVEN"

---
+++ ProyectoTI/src/main/java/com/unsis/edu/proyectoti/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Universidad de la Sierra Sur
+ * Lic Informática
+ * Luis Fernando Santiago Martínez
+ */
+package com.unsis.edu.proyectoti;
+
+/**
+ * @author Fercho
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con MAVEN");
+    }
+}
